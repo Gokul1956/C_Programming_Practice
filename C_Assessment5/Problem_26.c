5187ac9907da3940ef76af380840f6622e33ef51
Gokul1956 <gokulakrishnanmsd7@gmail.com>
2026-06-02 19:34:34 +0530

    Update Problem_26.c

--- C_Assessment5/Problem_26.c
+++ C_Assessment5/Problem_26.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int max=0;
-    for(int i = 1;i<100000;i++){
+    for(int i = 1;i<10000;i++){
         if(i%7== 0){
             if(i%9 ==0){
                 max=i;

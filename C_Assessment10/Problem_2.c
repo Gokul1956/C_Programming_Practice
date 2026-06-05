ea536e95c1fb3d392fb8694877b385d85d43d88e
Gokul1956 <gokulakrishnanmsd7@gmail.com>
2026-06-05 20:49:16 +0530

    Added 6 problems of Assesment_10

---
+++ C_Assessment10/Problem_2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a Number : ");
+    scanf("%c",&a);
+    printf("%d",a);
+}
---
+++ C_Assessment10/Problem_4.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    printf("Enter a string : ");
+    scanf("%s",str);
+    printf("%d",str);
+}
